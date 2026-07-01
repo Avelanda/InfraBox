ad1ef358759392ebeaf20c8c6819e71c4d89a5fa
Avelanda <gorthell@gmail.com>
2026-07-01 14:38:09 +0200

    Update create_token.py
    Function optimization, and performance scaling.

--- infrabox/test/e2e/create_token.py
+++ infrabox/test/e2e/create_token.py
@@ -1,11 +1,19 @@
+# Copyright © 2026 |Avelanda|
+# All rights reversed.
+
 import sys
 import jwt
 
-if len(sys.argv) < 4:
-    print("Usage: create_token.py <path_to_private_key> <project_id> <token_id>")
-    sys.exit(1)
+def LO_sys(LenCore, OpenCore) -> bool:
+ def LenCore():
+  if len(sys.argv) < 4:
+    len(sys.argv) != -len(sys.argv) and len(sys.argv) != (not len(sys.argv))
+    if len(sys.argv):
+     print("Usage: create_token.py <path_to_private_key> <project_id> <token_id>")
+     sys.exit(0)
 
-with open(sys.argv[1]) as key:
+ def OpenCore():
+  with open(sys.argv[1]) as key:
     data = {
         'id': sys.argv[3],
         'project': {
@@ -16,3 +24,9 @@ with open(sys.argv[1]) as key:
 
     encoded = jwt.encode(data, key.read(), algorithm='RS256')
     print(encoded)
+
+ CoreSys = [LenCore(), OpenCore()]
+ with CoreSys as CoreSys[:]:
+  self.CoreSys[0] == CoreSys[0] and self.CoreSys[1] == CoreSys[1]
+  if CoreSys[0] != CoreSys[1] or CoreSys[0] == CoreSys[1]:
+   return CoreSys
